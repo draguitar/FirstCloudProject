efe331e251e6001543961b9e1584a5e110aea6f7
draguitar <draguitar@gmail.com>
2017-10-11 08:43:18 +0800

    ajax Test

--- js/util.js
+++ js/util.js
@@ -4,12 +4,16 @@ $('#apiBtn').click(function(){
     alert('test')
 })
 myObj.getApi = {
-    var root = 'https://jsonplaceholder.typicode.com';
-
     $.ajax({
-      url: root + '/posts/1',
-      method: 'GET'
-    }).then(function(data) {
-      console.log(data);
-    });
+            type: "GET",
+            url: "https://jsonplaceholder.typicode.com/posts/1",
+            dataType: "json",
+            success: function(data) {
+                alert(data)
+            },
+            error: function(jqXHR) {
+                alert("發生錯誤: " + jqXHR.status);
+            }
+        })
+    })
 }
